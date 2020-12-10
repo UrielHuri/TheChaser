7cf1c56ce4d1f7aea266ac51d77e3cb368eb9ac2
LAPTOP-O7MVLFSV\uriel <urielhuri@gmail.com>
2020-12-10 08:27:32 +0200

    removed player and chaser management functions (were added to their classes). changed all prints to socket msgs

--- ChaserGame.py
+++ ChaserGame.py
@@ -1,101 +1,29 @@
-import pandas as pd
 from Question import *
 
 INVALID_INPUT = 'INVALID INPUT'
+MSG_SIZE = 1024
 
-def setChaserDiff(level):
-	return 100-(4-level)*10
+def getPlayerAnswer(conn,quetion, is_chase = False):
+	msg = str(quetion)
+	if is_chase:
+		msg += '\n(Enter L to use 50/50 lifebuoy)'
+	conn.send(msg.encode())
+	return conn.recv(1024).decode()
 
-class Player:
-	def __init__(self,name,account=0,lifebuoy = True):
-		self.name = name
-		self.account = account
-		self.lifebuoy = lifebuoy
-		self.availble_questions = createDBFromJson()
-
-	def deposite(self,amount):
-		self.account += amount
-
-	def use_question(self,q_index):
-		return self.availble_questions.pop(q_index)
-
-	def get_account(self):
-		return f'{self.account}$'
-
-	def use_lifebuoy(self):
-		self.lifebuoy = False
-
-
-class Board:
-	def __init__(self,levels = 8,player = 3,chaser = 0):
-		self.levels = levels
-		self.chaser = chaser
-		self.player = player
-
-	board = pd.DataFrame()
-
-	winner = None
-
-	def get_board(self):
-		levels_list = ['']*self.levels
-		levels_list[self.player] = "Player"
-		levels_list[self.chaser] = "Chaser"
-		self.board = pd.DataFrame.from_dict({'Board' : levels_list})
-		return self.board
-
-	def __repr__(self):
-		return f'Board({self.levels},{self.player},{self.chaser})'
-
-	def update_chaser(self):
-		self.chaser -= 1
-
-	def update_player(self):
-		self.player -= 1
-
-	def check_finish(self):
-		if self.chaser == self.player:
-			self.winner = 'Chaser'
-		elif self.player == -1:
-			self.winner = 'Player'
-		if self.winner:
-			return f'{self.winner} Won!'
-		return False
-
-	def chaser_offers(self,amount):
-		self.get_board()
-		change_amount_by_ratio = 2
-		self.board.loc[self.player-1,'Board'] = amount*change_amount_by_ratio
-		self.board.loc[self.player+1,'Board'] = amount//change_amount_by_ratio
-		return self.board
-
-def getPlayerAnswer(quetion):
-	print(quetion)
-	print('(Enter L to use 50/50 lifebuoy)')
-	return input('Answer: ').upper()
-
-def getChaserAnswer(question, rightness_per = 75):
-	chance = rint(1,100)
-	if chance <= rightness_per:
-		return question.answer
-	else:
-		options = question.options.keys()
-		options.pop(question.answer)
-		i = rint(0,len(options)-1)
-		return options[i]
-
-def initialization():
-	print('Welcome to The Chaser!')
-	wanna_play = input("Do you want to play? (Y/N)").upper()
+def initialization(conn):
+	conn.send(b'Welcome to The Chaser!\n')
+	conn.send(b"Do you want to play? (Y/N)")
+	wanna_play = conn.recv(MSG_SIZE).decode().upper()
 	if wanna_play == 'Y':
 		return True
 	elif wanna_play == 'N':
 		return False
 	else:
-		print(INVALID_INPUT)
-		return initialization()
+		conn.send(INVALID_INPUT.encode())
+		return False
 
-def phaseOne(player):
-	print("Welcome to phase #1 of The Chaser!\nLet's start:\n")
+def phaseOne(conn,player):
+	conn.send(b"Welcome to phase #1 of The Chaser!\nLet's start:\n")
 	REQUESTED_QUESTIONS = 3
 	RIGHT_ANSWER_REWARD = 5000
 	RIGHT_ANSWER_ANNOUNCE = 'Nice one! your answer is right and you get {}$.\nYour current accunt status is {}.'
@@ -104,63 +32,64 @@ def phaseOne(player):
 		num = getQuestionNum(player.availble_questions)
 		question = player.availble_questions[num]
 		player.use_question(num)
-		answer = getPlayerAnswer(question)
+		answer = getPlayerAnswer(conn,question)
 		if question.is_right(answer):
 			player.deposite(RIGHT_ANSWER_REWARD)
-			print(RIGHT_ANSWER_ANNOUNCE.format(RIGHT_ANSWER_REWARD,player.get_account()))
+			conn.send(RIGHT_ANSWER_ANNOUNCE.format(RIGHT_ANSWER_REWARD,player.get_account()).encode())
 		else:
-			print(WRONG_ANSWER_ANNOUNCE.format(player.get_account()))
+			conn.send(WRONG_ANSWER_ANNOUNCE.format(player.get_account()).encode())
 	if player.account > 0:
 		return True
 	else:
-		print("You haven't got any right answer...\nYOU LOST and you are a loser.")
-		return initialization()
+		conn.send(b"You haven't got any right answer...\nYOU LOST and you are a loser.\n\n")
+		return False
 
-def handlingPlayerChoise(choise,player,board):
-	if choise not in ['0','1','2']:
+def handlingPlayerChoice(conn, player_choice,player,board):
+	if player_choice not in ['0','1','2']:
 		print(INVALID_INPUT)
-		return phaseTwo(player,board)
-	if choise == '1':
+		return phaseTwo(conn, player,board)
+	if player_choice == '1':
 		player.deposite(-player.account/2)
 		board.player += 1
-	elif choise == '2':
+	elif player_choice == '2':
 		player.deposite(player.account)
 		board.player -= 1
 
 
-def theChase(player,board):
+def theChase(conn, player, chaser, board):
 	while not board.check_finish():
 		q_num = getQuestionNum(player.availble_questions)
 		q = player.use_question(q_num)
-		player_input = getPlayerAnswer(q)
+		player_input = getPlayerAnswer(conn,q)
 		if player_input == 'L':
 			player.use_lifebuoy(q)
-			player_input = getPlayerAnswer(q.use_lifebuoy())
+			player_input = getPlayerAnswer(conn,q.use_lifebuoy())
 		player_ans = q.is_right(player_input)
-		chaser_ans = q.is_right(q, getChaserAnswer(q))
+		chaser_ans = q.is_right(chaser.get_answer(q))
 		if player_ans:
-			print('Player was Right!')
+			conn.send(b'Player was Right!\n')
 			board.update_player()
 		else:
-			print('Player was Wrong...')
+			conn.send(b'Player was Wrong...\n')
 		if chaser_ans:
-			print('Chaser was Right!')
+			conn.send(b'Chaser was Right!\n')
 			board.update_chaser()
 		else:
-			print('Chaser was Wrong...')
-		print(board.get_board())
+			conn.send(b'Chaser was Wrong...\n')
+		conn.send(str(board.get_board()).encode())
 
-def phaseTwo(player, board):
-	print("Nice! you made it to phase #2!")
+def phaseTwo(conn, player, chaser, board):
+	conn.send(b"Nice! you made it to phase #2!\n")
 	RATIO_OF_AMOUNT_CHANGE = 2
-	print("You can start 2 steps away from the chaser with your current amount: {}.".format(player.get_account()))
-	print("However, the chaser is willing to offer you two alternatives:")
-	print("(1) You can go one step DOWN and play for HALF of the amount you have now, which is {}$".format(
-		RATIO_OF_AMOUNT_CHANGE // player.account))
-	print("(2) You can go one step UP and play on TWICE the amount you have now, which is {}$".format(
-		RATIO_OF_AMOUNT_CHANGE * player.account))
-	print(board.chaser_offers(player.account))
-	choise = input("SO what will you choose?\n(enter 0 to continue with your current amount)\n")
-	handlingPlayerChoise(choise,player,board)
-	print(board.get_board())
-	theChase(player,board)
+	conn.send("You can start 2 steps away from the chaser with your current amount: {}.\n".format(player.get_account()).encode())
+	conn.send(b"However, the chaser is willing to offer you two alternatives:\n")
+	conn.send("(1) You can go one step DOWN and play for HALF of the amount you have now, which is {}$\n".format(
+		RATIO_OF_AMOUNT_CHANGE // player.account).encode())
+	conn.send("(2) You can go one step UP and play for TWICE the amount you have now, which is {}$\n".format(
+		RATIO_OF_AMOUNT_CHANGE * player.account).encode())
+	conn.send(str(board.chaser_offers(player.account)).encode())
+	conn.send(b"SO what will you choose?\n(enter 0 to continue with your current amount)\n")
+	player_choice = conn.recv(1024).decode()
+	handlingPlayerChoice(conn, player_choice, player, board)
+	conn.send(str(board.get_board()).encode())
+	theChase(conn, player, chaser, board)
