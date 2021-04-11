4dc41521ae764fc1b0fc0ae4c7425c60b83644f8
LAPTOP-O7MVLFSV\uriel <urielhuri@gmail.com>
2021-04-12 00:26:52 +0300

    initial GUI work - phase one window: added text for question and 4 buttons for answers

--- PhaseOne.py
+++ PhaseOne.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 from Utils import WINDOW_WIDTH, WINDOW_HEIGHT
+from Question import Question
 
 WINDOW_TITLE = 'PhaseOne: FirstCollection'
 
 class PhaseOne:
-	def __init__(self,question_num = 1):
+	def __init__(self,question : Question, question_num = 1):
 		root = tk.Tk()
 		# set window properties
 		root.title(WINDOW_TITLE)
@@ -16,21 +17,45 @@ class PhaseOne:
 		title = tk.Label(root)
 		title['background'] = 'black'
 		title['foreground'] = 'white'
-		title['text'] = f'Question #{question_num}:'
+		title['text'] = f'PHASE ONE:'
 		title['font'] = ("Broadway", 50)
-		title.pack(side='left')
+		title.pack(side='top')
 
-		# new game button
-		new_game_btn = tk.Button(root)
-		new_game_btn['bg'] = 'black'
-		new_game_btn['foreground'] = 'white'
-		new_game_btn['text'] = 'NEW GAME'
-		new_game_btn['font'] = ("Broadway", 30)
-		new_game_btn['command'] = newGame
-		new_game_btn.place(relx=0.5, rely=0.5, anchor='center')
+		# question title
+		question_title = tk.Label(root)
+		question_title['background'] = 'black'
+		question_title['foreground'] = 'white'
+		question_title['text'] = f'Question #{question_num}:'
+		question_title['font'] = ("Broadway", 30)
+		question_title.pack(side='left')
+
+		# question text field
+		question_field = tk.Text(root)
+		question_field.place(x=300, y=400, anchor='w')
+		question_field.insert(tk.INSERT,question.question)
+		question_field['height'] = 3
+		question_field['width'] = 30
+		question_field['font'] = ("Broadway", 25)
+
+		x, y = 400, 600
+		i = 0
+		for ans in question.options:
+			ans_field = tk.Button(root)
+			ans_field['text'] = f'{ans}. {question.options[ans]}'
+			if i % 2 == 1:
+				x+=200
+			else:
+				x = 400
+			if i > 1:
+				y = 700
+			ans_field.place(x=x,y=y,anchor='center')
+			i+=1
 
 		root.mainloop()
 
 def newGame():
 	print('PRESS')
-wind = PhaseOne()
+
+q = Question('What is the answer?',{'A': '1', 'B': '2', 'C': '3', 'D': '4'},'B')
+
+wind = PhaseOne(q)
